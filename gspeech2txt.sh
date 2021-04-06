a12eed52bb8ad8d8612acbd09945bc72d453fa61
dr-ni <68241100+dr-ni@users.noreply.github.com>
2021-04-06 14:02:04 +0200

    Update gspeech2txt.sh

--- gspeech2txt.sh
+++ gspeech2txt.sh
@@ -15,11 +15,14 @@ KEY="......your-api-key......."
 START="transcript\": \""
 END="\","
 
+rm -f audiofile.flac
+rm -f audiofile.base64
 
-rec -c1 file.flac vad -s 1 -t 7 -T 0.2 -g 0.5 silence -l 0 0 0:00:03 1% rate $SAMPLE_RATE
+rec -c1 audiofile.flac vad -s 1 -t 7 -T 0.2 -g 0.5 silence -l 0 0 0:00:03 1% rate $SAMPLE_RATE
 #play -c1 file.flac 
-base64 file.flac -w 0 > file.flac.base64
-base64_flac=$(cat file.flac.base64)
+base64 audiofile.flac -w 0 > audiofile.base64
+base64_flac=$(cat audiofile.base64)
+
 echo '{"audio":{"content": "'$base64_flac'"}, "config":{"encoding":"FLAC","audio_channel_count":"1","sample_rate_hertz":"'$SAMPLE_RATE'","language_code":"de-DE"}}' > body.txt
 curl -o stt.txt -X POST -H "Content-Type: application/json" -d @body.txt "https://speech.googleapis.com/v1/speech:recognize?key=$KEY" >/dev/null 2>&1 | sed -e 's/[{}]/''/g' | awk -F\":\" '{print $4}' | awk -F\",\" '{print $1}' | tr -d '\\n' >/dev/null 2>&1
 
